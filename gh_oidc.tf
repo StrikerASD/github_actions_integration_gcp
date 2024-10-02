805c62903946b3e2f757642236235fcb99ba9f0d
Iwo <igi31@wp.pl>
2024-10-02 12:21:17 +0200

    Removed pipeline because actions needs to be done from local computer

--- gh_oidc.tf
+++ gh_oidc.tf
@@ -2,8 +2,8 @@ module "gh_oidc" {
   source            = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
   version           = "v3.1.1"
   project_id        = var.project_id
-  pool_id           = "gh-identity-pool"
-  pool_display_name = "Identity Pool"
+  pool_id           = "gh-identity-pool-second"
+  pool_display_name = "Identity Pool Second"
   provider_id       = "gh-provider"
   sa_mapping = {
     (google_service_account.github-svc.account_id) = {
