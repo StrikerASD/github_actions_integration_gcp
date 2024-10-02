dd8aa6e70f812ae3b40cdb095f43a946bc8aa2f2
Iwo <igi31@wp.pl>
2024-10-02 09:55:24 +0200

    Commented wif_api resource because pipeline is failing

--- enable_apis.tf
+++ enable_apis.tf
@@ -1,11 +1,11 @@
-resource "google_project_service" "wif_api" {
-  for_each = toset([
-    "iam.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
-    "iamcredentials.googleapis.com",
-    "sts.googleapis.com",
-  ])
-  project            = var.project_id
-  service            = each.value
-  disable_on_destroy = false
-}
+# resource "google_project_service" "wif_api" {
+#   for_each = toset([
+#     "iam.googleapis.com",
+#     "cloudresourcemanager.googleapis.com",
+#     "iamcredentials.googleapis.com",
+#     "sts.googleapis.com",
+#   ])
+#   project            = var.project_id
+#   service            = each.value
+#   disable_on_destroy = false
+# }
