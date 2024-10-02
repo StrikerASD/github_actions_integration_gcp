8f25cfa68f29635e5682c5260e804cc3d38918e5
Iwo <igi31@wp.pl>
2024-10-02 09:57:24 +0200

    Commented gh_oidc because resources are not creating

--- gh_oidc.tf
+++ gh_oidc.tf
@@ -1,14 +1,14 @@
-module "gh_oidc" {
-  source            = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
-  version           = "v3.1.1"
-  project_id        = var.project_id
-  pool_id           = "gh-identity-pool"
-  pool_display_name = "Identity Pool"
-  provider_id       = "gh-provider"
-  sa_mapping = {
-    (google_service_account.github-svc.account_id) = {
-      sa_name   = google_service_account.github-svc.name
-      attribute = "*"
-    }
-  }
-}
+# module "gh_oidc" {
+#   source            = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+#   version           = "v3.1.1"
+#   project_id        = var.project_id
+#   pool_id           = "gh-identity-pool"
+#   pool_display_name = "Identity Pool"
+#   provider_id       = "gh-provider"
+#   sa_mapping = {
+#     (google_service_account.github-svc.account_id) = {
+#       sa_name   = google_service_account.github-svc.name
+#       attribute = "*"
+#     }
+#   }
+# }
